c119776790c354dfbc8351b969c7b0e22fc259a4
Diego Minelli <diego.minelli@hotmail.com>
2022-08-27 15:03:43 -0300

    criacao da classe usuario

---
+++ android/app/src/main/kotlin/com/example/minhanovaaplicacao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.minhanovaaplicacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,29 @@
+import 'package:minhanovaaplicacao/models/user.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_USERS = {
+  '1': User(
+      id: '1',
+      nome: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png"),
+  '2': User(
+      id: '1',
+      nome: 'Lucas',
+      email: 'lucas@alunos.com.br',
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png"),
+  '3': User(
+      id: '1',
+      nome: 'Gil',
+      email: 'gil@alunos.com.br',
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png"),
+  '4': User(
+      id: '1',
+      nome: 'Fabiano',
+      email: 'fabiano@alunos.com.br',
+      avatarUrl:
+          "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png"),
+};
